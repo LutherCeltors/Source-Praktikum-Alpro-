095baeaad96b65bd263353aa223152cc2ca783f2
Ishakpln <isaacnapz777999@gmail.com>
2025-03-19 01:15:30 +0700

    modify

--- Prak_1/ishak/prakno5.c
+++ Prak_1/ishak/prakno5.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 
 int main() {
-    int n;
+    int N;
+    int x;
+    int init = 1;
+    int add;
     
-    // Taking user input for matrix size
-    printf("Enter the size of the matrix (n x n): ");
-    scanf("%d", &n);
-
-    int arr[n][n]; 
-    int num = 1;
-
-    // Filling diagonally from top-left to bottom-right (row-wise)
-    for (int d = 0; d < 2 * n - 1; d++) {
-        int row = (d < n) ? d : n - 1;
-        int col = (d < n) ? 0 : d - n + 1;
+    scanf("%d", &N);
+    
+    for (int i = 0; i < N; i = i + 1) {
+        x = init;
+        add = i;
         
-        while (row >= 0 && col < n) {
-            arr[col][row] = num++;  // Swap row and col to get correct order
-            row--;
-            col++;
+        for (int j = 0; j < N; j = j + 1) {
+            if (j < N - i - 1) {
+                printf("%d", x);
+                add = add + 1;
+                x = x + add;
+            } else {
+                printf("%d", x);
+                if (j != N - i - 1) {
+                    add = add - 1;
+                }
+                x = x + add;
+            }
+            printf(" ");
         }
-    }
-
-    // Printing the matrix
-    printf("Generated %d x %d pattern:\n", n, n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%2d ", arr[i][j]);
-        }
+        
         printf("\n");
+        init = init + i + 2;
     }
-
+    
     return 0;
 }
