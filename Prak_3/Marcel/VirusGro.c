8804fcf850e77d85459fba9ab3c96eeac5d9695d
LutherCeltors <lutherceltors@gmail.com>
2025-04-08 22:37:47 +0700

    Adding Virus Gro

--- Prak_3/Marcel/VirusGro.c
+++ Prak_3/Marcel/VirusGro.c
@@ -1,12 +1,64 @@
 #include <stdio.h>
 
 int main(){
-    int n, k, efect;
+    int efektivitas (int arr[],int sorted[], int jumlah_virus, int n);
+    void sorting (int sorted[], int n);
+    int n, k, effect, temp_effect, hasil;
     int gkanan, gkiri;
     int arr[1001];
+    int sorted[1001];
 
     scanf("%d %d", &n, &k);
     for(int i = 0; i<n; i++) scanf("%d", &arr[i]);
 
-    for (int j = 0; j<n; j++) ;
+    //copy ke sorted
+    // for(int i = 0; i<n; i++) sorted[i] = arr[i];
+    // sorting(sorted, n);
+    // for (int i = 0; i<n; i++) printf("%d\n", sorted[i]);
+    /*cari maksimum apabila titik sebar virus hanya satu saja*/
+    // hasil = efektivitas(arr, sorted, k, n);
+    // printf("%d\n", hasil); 
+
+    effect = 0;
+    gkiri = arr[0];
+    gkanan = arr[n-1];
+    effect = gkiri + gkanan;
+    for (int j = 1; j<n-1; j++){
+        temp_effect = gkiri + gkanan + arr[j];
+        if (temp_effect > effect) effect = temp_effect;
+    }
+    printf("%d\n", effect);
+   
+}
+
+int efektivitas (int arr[],int sorted[], int jumlah_virus, int n){
+    int effect, temp_effect;
+    int gkanan, gkiri;
+    if (jumlah_virus== 1){
+        effect = 0;
+        gkiri = arr[0];
+        gkanan = arr[n-1];
+        effect = gkiri + gkanan;
+        for (int j = 1; j<n-1; j++){
+            temp_effect = gkiri + gkanan + arr[j];
+            if (temp_effect > effect) effect = temp_effect;
+        }
+    }else if(jumlah_virus> 1);
+    return effect;
+}
+void sorting (int sorted[], int n){
+    int temp, pass;
+    int tukar = 1;
+    while (n>1 && tukar == 1){
+        tukar = 0;
+        pass = 0;
+        for (int i = n; i > pass + 1; i--){
+            if (sorted[i] > sorted[i-1]){
+                temp = sorted[i];
+                sorted[i] = sorted[i-1];
+                sorted[i-1] = temp;
+                tukar = 1;
+            }
+        } pass += 1;   
+    }
 }
--- Prak_3/Marcel/tempCodeRunnerFile.c
+++
@@ -1,8 +0,0 @@
-    temp = 0;
-    for (int j = 1; j<= rot; j++){
-        for (int k = N-1; j>1; j--){
-            temp = arr[k];
-            arr[k] = arr[k-1];
-            arr[k-1] = temp;
-        }
-    }
