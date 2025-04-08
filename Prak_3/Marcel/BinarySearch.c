c2e918258d81e735916fb955efb22e7f22381dc1
LutherCeltors <lutherceltors@gmail.com>
2025-04-08 20:32:25 +0700

    Refactoring BinarySearch

--- Prak_3/Marcel/BinarySearch.c
+++ Prak_3/Marcel/BinarySearch.c
@@ -1,44 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(){
-    int sumi, bil, idx, k;
-    scanf("%d", &sumi);
-    int n[sumi], sumk[50];
-    for (int i = 0; i<=sumi; i++)
-        scanf("%d", &n[i]);
-    do{
-        scanf("%d", &bil);
-        k = 0;
-        int idxl = -2;
-        for (int j=0 ; j<=sumi; j++){
-            printf("%d", j);
-            if (n[j] == bil){
-                k = k + 1;
-                idx = j;  
-                if (idxl != idx)
-                    sumk[k] = idx;  
-                idxl = idx; 
-                printf("-%d-\n", idx);
-                printf("I%dI\n", k);
-                break;
-            }
-        }if (k==0){
-            idx = -1;
-            k = k + 1;
-            sumk[k] = idx;
-            printf("I%dI\n", k);
-            printf("-%d-\n", idx);
-        }
-    }while(idx != -1);
+    int N, arr[1001], tot_search, arrhasil[1001], search;
+    int baw, bak, bat; /*batas awal, akhir dan tengah*/
 
-    printf("[");
-    for (int l=1; l<= k; l++){
-        printf("%d", sumk[l]);
-        if (l != k){
-            printf(", ");
+    scanf("%d", &N);
+    for (int i = 0; i < N; i++) scanf("%d", &arr[i]);
+    scanf("%d", &tot_search);
+
+    for (int j = 0; j < tot_search; j++){
+        scanf("%d", &search);
+        baw = 0;
+        bak = N-1;
+        arrhasil[j] = -1;
+
+        while (bak >= baw){
+            bat = (baw+bak)/2;
+            if (arr[bat] == search){
+                arrhasil[j] = bat;
+                bak = bat - 1;
+            }else if(search > arr[bat]){
+                baw = bat + 1;
+            }else if(search < arr[bat]){
+                bak = bat - 1;
+            }
         }
     }
-    printf("]\n");
-    
+    printf("[");
+    for (int k = 0; k<tot_search; k++) {
+        if (k != tot_search -1) printf("%d, ", arrhasil[k]);
+        else printf("%d]\n", arrhasil[k]);
+    }
 }
---
+++ Prak_3/Marcel/VirusGro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+    int n, k, efect;
+    int gkanan, gkiri;
+    int arr[1001];
+
+    scanf("%d %d", &n, &k);
+    for(int i = 0; i<n; i++) scanf("%d", &arr[i]);
+
+    for (int j = 0; j<n; j++) ;
+}
