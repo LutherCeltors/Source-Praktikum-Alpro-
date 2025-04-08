811ae22f65c2e5b90a43cc6f1779fad383614629
LutherCeltors <lutherceltors@gmail.com>
2025-04-08 18:09:23 +0700

    Adding kputaran.c

--- Prak_3/Marcel/kputaran.c
+++ Prak_3/Marcel/kputaran.c
@@ -1,1 +1,24 @@
+#include <stdio.h> 
 
+int main(){
+    int N,rot,temp;
+    int arr[100];
+    scanf("%d %d", &N, &rot);
+
+    for(int i = 0; i<N; i++) scanf("%d", &arr[i]);
+
+    temp = 0;
+    rot = rot % N;
+    for (int j = 1; j<= rot; j++){
+        for (int k = N-1; k>0; k--){
+            temp = arr[k];
+            arr[k] = arr[k-1];
+            arr[k-1] = temp;
+        }
+    }
+
+    for (int l = 0; l<N; l++){
+        if (l != N-1) printf("%d ", arr[l]);
+        else printf("%d\n", arr[l]);
+    }
+}
--- Prak_3/Marcel/tempCodeRunnerFile.c
+++ Prak_3/Marcel/tempCodeRunnerFile.c
@@ -1,2 +1,8 @@
-    // printf("even: %d\n", even);
-    // printf("odd: %d\n", odd);
+    temp = 0;
+    for (int j = 1; j<= rot; j++){
+        for (int k = N-1; j>1; j--){
+            temp = arr[k];
+            arr[k] = arr[k-1];
+            arr[k-1] = temp;
+        }
+    }
